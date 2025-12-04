ef82f63f1d9e7232944400347c726b04ec84e2c5
KleitonBarone <25616578+KleitonBarone@users.noreply.github.com>
2025-12-04 02:29:16 -0300

    feat: Remove `Todolist` and `Placargol` components and update `package.json` preview script.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import './App.css';
 import Conversor from './components/Conversor'
-import Placargol from './components/Placargol'
-import Todolist from './components/Todolist'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Conversor de Moedas</h1>
       <div className="linha">
-        <Conversor moedaA="USD" moedaB="BRL"/>
-        <Conversor moedaA="BRL" moedaB="USD"/>
+        <Conversor moedaA="USD" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="USD" />
       </div>
       <div className="linha">
-        <Conversor moedaA="CAD" moedaB="BRL"/>
-        <Conversor moedaA="BRL" moedaB="CAD"/>
+        <Conversor moedaA="CAD" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="CAD" />
       </div>
       <div className="linha">
-        <Conversor moedaA="EUR" moedaB="BRL"/>
-        <Conversor moedaA="BRL" moedaB="EUR"/>
+        <Conversor moedaA="EUR" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="EUR" />
       </div>
-      <div className="linha">
-        <Placargol timeA="Flamengo" timeB="Vasco"/>
-      </div>
-      <div className="linha">
-        <Todolist/>
-      </div>
+
     </div>
   );
 }
